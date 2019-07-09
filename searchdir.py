f20a8576a884e3710255bb6f4113eab4baf16836
Arasy <Arasy@users.noreply.github.com>
2019-07-10 04:42:53 +0700

    Add files via upload

---
+++ searchdir.py
@@ -0,0 +1,27 @@
+import os
+import csv
+
+rootdir = os.getcwd()
+
+print('Searching...')
+data = []
+for r,d,f in os.walk(rootdir):
+    for file in f:
+        try:
+            data.append([r,file,str(os.path.getsize(os.path.join(r,file)))])
+        except FileNotFoundError:
+            data.append((r,file,'unk'))
+
+print('Saving to file...')
+f = open('index.txt','w')
+for i in data:
+    f.write('\t'.join(i))
+    f.write('\n')
+f.close()
+
+#csv
+with open("index.csv","w") as csvfile:
+    wr = csv.writer(csvfile,quoting=QUOTE_ALL)
+    wr.writerow(data)
+
+print('Finished!')
