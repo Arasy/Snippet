5d34800b63bf4969ac48eff77f4f07d9e1d7e5d9
Arasy <Arasy@users.noreply.github.com>
2019-07-23 11:51:30 +0700

    Restruktur

--- searchdir.py
+++ searchdir.py
@@ -2,30 +2,41 @@ import os
 import csv
 import sys
 
-rootdir = os.getcwd()
+def crawldir(rootdir):
+    print('Searching...')
+    data = []
+    for r,d,f in os.walk(rootdir):
+        for file in f:
+            try:
+                data.append([r,file,str(os.path.getsize(os.path.join(r,file)))])
+            except FileNotFoundError:
+                data.append((r,file,'unk'))
+    return data
 
-if sys.argv[1]:
-    rootdir = sys.argv[1]
+def savefile(data):
+    print('Saving to file...')
+    name = 'index'
+    if sys.argv[2]:
+        name = 'index'+sys.argv[2]
 
-print('Searching...')
-data = []
-for r,d,f in os.walk(rootdir):
-    for file in f:
-        try:
-            data.append([r,file,str(os.path.getsize(os.path.join(r,file)))])
-        except FileNotFoundError:
-            data.append((r,file,'unk'))
+    f = open(name+'.txt','w',encoding='utf-8')
+    for i in data:
+        f.write('\t'.join(i))
+        f.write('\n')
+    f.close()
 
-print('Saving to file...')
-f = open('index.txt','w',encoding='utf-8')
-for i in data:
-    f.write('\t'.join(i))
-    f.write('\n')
-f.close()
+    #csv
+    with open(name+".csv","w",encoding="utf-8") as csvfile:
+        wr = csv.writer(csvfile)
+        wr.writerows(data)
 
-#csv
-with open("index.csv","w",encoding="utf-8") as csvfile:
-    wr = csv.writer(csvfile)
-    wr.writerows(data)
 
-print('Finished!')
+if __name__ == "__main__":
+    rootdir = os.getcwd()
+
+    if sys.argv[1]:
+        rootdir = sys.argv[1]
+
+    data = crawldir(rootdir)
+    savefile(data)
+    print('Finished!')
