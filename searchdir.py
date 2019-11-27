6efd12013fe9a2f0719484d861c6bfd5d8274540
Arasy <Arasy@users.noreply.github.com>
2019-11-27 10:46:49 +0700

    Save to file using pandas

--- searchdir.py
+++ searchdir.py
@@ -1,23 +1,37 @@
 import os
 import csv
 import sys
+import pandas as pd
+import zipfile
 
 def crawldir(rootdir):
     print('Searching...')
     data = []
     for r,d,f in os.walk(rootdir):
         for file in f:
+            #informasi dasar file : path, file, size
+            fullpath = os.path.join(r,file)
+            content = []
             try:
-                data.append([r,file,str(os.path.getsize(os.path.join(r,file)))])
+                info = [r,file,str(os.path.getsize(fullpath))]
             except Exception :
-                data.append((r,file,'unk'))
+                info = [r,file,'unk']
+            if(zipfile.is_zipfile(fullpath)):
+                info.append(True)
+                a = zipfile.ZipFile(fullpath)
+                for fz in a.namelist():
+                    content.append([fullpath,a.getinfo(fz).filename,a.getinfo(fz).file_size,'isi'])
+            else:
+                info.append(False)
+            data.append(info)
+            data.extend(content)
     return data
 
 def savefile(data):
     print('Saving to file...')
     name = 'index'
-    if sys.argv[2]:
-        name = 'index'+sys.argv[2]
+#    if sys.argv[2]:
+#        name = 'index'+sys.argv[2]
 
     f = open(name+'.txt','w',encoding='utf-8')
     for i in data:
@@ -25,18 +39,21 @@ def savefile(data):
         f.write('\n')
     f.close()
 
-    #csv
-    with open(name+".csv","w",encoding="utf-8") as csvfile:
-        wr = csv.writer(csvfile)
-        wr.writerows(data)
-
+#    #csv
+#    with open(name+".csv","w",encoding="utf-8") as csvfile:
+#        wr = csv.writer(csvfile)
+#        wr.writerows(data)
 
 if __name__ == "__main__":
     rootdir = os.getcwd()
 
-    if sys.argv[1]:
-        rootdir = sys.argv[1]
+#    if sys.argv[1]:
+#        rootdir = sys.argv[1]
 
     data = crawldir(rootdir)
-    savefile(data)
+
+    df = pd.DataFrame(data)
+    df.to_csv("index.csv")
+
+#    savefile(data)
     print('Finished!')
